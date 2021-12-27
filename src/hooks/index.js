7fb8deeb345a964fce369828b2e740bbf971c03a
briwa <shabriwa.shalat@gmail.com>
2021-12-28 03:03:13 +0900

    Updates

--- src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export * from './canvas';
-export * from './base-image';
-export * from './keys';
-export * from './viewport';
-export * from './active-item';
-export * from './download';
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import canvasReducer from './slices/canvas';
-
-export default configureStore({
-  reducer: {
-    canvas: canvasReducer,
-  },
-});
